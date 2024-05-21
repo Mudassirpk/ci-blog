fb915004a638441c401bf1eb417eea0b669daef9
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-21 16:03:39 +0500

    added author id and auto timestamps

--- app/Models/PostsModel.php
+++ app/Models/PostsModel.php
@@ -8,10 +8,13 @@ class PostsModel extends Model
 {
     protected $table = "posts";
 
-    protected $allowedFields = ['title', 'description'];
+    protected $allowedFields = ['title', 'description', 'author_id'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
 
     public function getPosts(): array
     {
-        return $this->findAll();
+        return $this->select('posts.*, users.username as author')->join('users', 'users.id=posts.author_id')->findAll();
     }
 }
