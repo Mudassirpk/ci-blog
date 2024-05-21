887f72436aa16fd7b19132a4047dfb6340b6805c
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-21 12:17:28 +0500

    added logout link and add logged in username

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url() ?>css/style.css">
-    <title><?= $title ?></title>
+    <title><?= /** @var TYPE_NAME $title */
+        $title ?></title>
 </head>
 
 <body class="w-full min-h-screen flex flex-col">
@@ -17,7 +18,12 @@
             <a href="/create">Create Post</a>
         </div>
         <?php if (session()->get('isLoggedIn')): ?>
-            <p class="text-indigo-600"><?= session()->get('email') ?></p>
+            <div class="flex gap-2 items-center">
+                <p class="text-indigo-900 bg-indigo-100 px-4 py-2 rounded-xl capitalize font-semibold"><?= session()->get('name') ?></p>
+                <div class="w-auto p-2 rounded-xl shadow-gray-300 shadow-sm border-t border-t-indigo-600">
+                    <a href="/logout" class="p-2 rounded-xl hover:bg-gray-200">Logout</a>
+                </div>
+            </div>
         <?php else: ?>
             <a href="/login"
                class="px-4 py-2 rounded-xl shadow-md hover:bg-indigo-500 shadow-indigo-200 bg-indigo-600 text-white">Login</a>
