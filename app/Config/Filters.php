ce8644e175de931774791c4c77c55517afe8b232
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-21 12:16:58 +0500

    fixed filter route

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -106,6 +106,6 @@ class Filters extends BaseFilters
      * @var array<string, array<string, list<string>>>
      */
     public array $filters = [
-        'auth'=>['before'=>'/createpost']
+        'auth'=>['before'=>'/create']
     ];
 }
