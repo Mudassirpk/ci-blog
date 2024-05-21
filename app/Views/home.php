ff508baf71ca66e870da9863e9cd8a6a42d8a8fe
Mudassir Sarfraz <91904527+Mudassirpk@users.noreply.github.com>
2024-05-21 16:03:20 +0500

    added created at and author name ui

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,13 +1,17 @@
 <h1 class="text-6xl w-full text-center text-indigo-600 font-bold">M-Blog</h1>
 
-<h2 class="font-semibold text-2xl">Posts</h2>
+<h2 class="font-semibold text-2xl my-4 border-b border-b-gray-200 pb-2">Posts</h2>
 
 <div class="w-full flex flex-col gap-4">
     <?php if (isset($posts) && $posts !== []): ?>
         <?php foreach ($posts as $post): ?>
             <div>
-                <h3 class="text-lg text-gray-800 font-semibold"><?= esc($post['title']) ?></h3>
+                <h3 class="capitalize text-2xl my-2 text-gray-800 font-semibold"><?= esc($post['title']) ?>
+                    | <?= esc($post['created_at']) ?>
+                </h3><span
+                        class="text-xl"><?= esc($post['author']) ?></span>
                 <p class="text-lg text-gray-700"><?= esc($post['description']) ?></p>
+                <div class="border-b border-gray-200 mt-6 mx-auto"></div>
             </div>
         <?php endforeach ?>
     <?php else: ?>
